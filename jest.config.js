575844504bc4b727cc5c47f1183ff02f90678245
Viacheslav Borisov <slavb18@iconicompany.com>
2025-05-22 16:26:59 +0000

    update

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,8 @@
 export default {
-  preset: 'ts-jest', // вместо просто 'ts-jest'
+  preset: 'ts-jest',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
   },
 };
